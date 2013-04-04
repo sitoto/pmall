40a1873035278f9258809ed246e66eb27dce3a9a
hw <ruby@AY1209240752420f19993.(none)>
2013-04-04 19:18:51 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pei::Application.config.secret_token = 'f0678801adbe0378ccceeb9f58671df92d681ceb35d2717b8d9f79440922619e4d0f46b5fc74facbe90de53d4048736780f0a94f5fbf739feab06e75699acf50'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Pei::Application.config.session_store :cookie_store, key: '_pei_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Pei::Application.config.session_store :active_record_store
